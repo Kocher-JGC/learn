6252fd836180fa7fca14b8935e7bad5bf251a4db
Kocher-JGC <879515820@qq.com>
2019-01-02 19:28:32 +0800

    添加render逻辑

--- frame_learn/vue2x/src/core/instance/render.js
+++ frame_learn/vue2x/src/core/instance/render.js
@@ -23,7 +23,8 @@ export function initRender (vm: Component) {
   // the placeholder node in parent tree // 父树中的占位符节点
   const parentVnode = vm.$vnode = options._parentVnode
   const renderContext = parentVnode && parentVnode.context // 渲染上下文
-  vm.$slots = resolveSlots(options._renderChildren, renderContext) // 解析插槽 ==> 内部
+  /** 解析插槽 ==> 内部 **/
+  vm.$slots = resolveSlots(options._renderChildren, renderContext)
   vm.$scopedSlots = emptyObject // 作用域插槽
   // bind the createElement fn to this instance
   // so that we get proper render context inside it.
@@ -42,6 +43,7 @@ export function initRender (vm: Component) {
   const parentData = parentVnode && parentVnode.data
 
   /* istanbul ignore else */
+  // 监听$attrs和$listeners
   if (process.env.NODE_ENV !== 'production') {
     defineReactive(vm, '$attrs', parentData && parentData.attrs || emptyObject, () => {
       !isUpdatingChildComponent && warn(`$attrs is readonly.`, vm)
@@ -57,6 +59,7 @@ export function initRender (vm: Component) {
 
 export function renderMixin (Vue: Class<Component>) {
   // install runtime convenience helpers
+  // 安装各种渲染的工具
   installRenderHelpers(Vue.prototype)
 
   Vue.prototype.$nextTick = function (fn: Function) {
@@ -65,32 +68,36 @@ export function renderMixin (Vue: Class<Component>) {
 
   Vue.prototype._render = function (): VNode {
     const vm: Component = this
+    // 拿到compiler生成的render和staticRender以及拿到init时候存储的父级vnode
     const { render, _parentVnode } = vm.$options
 
     // reset _rendered flag on slots for duplicate slot check
     if (process.env.NODE_ENV !== 'production') {
       for (const key in vm.$slots) {
-        // $flow-disable-line
+        // $flow-disable-line // 开发环境记录渲染状态
         vm.$slots[key]._rendered = false
       }
     }
 
-    if (_parentVnode) {
+    if (_parentVnode) { // 这里定义的作用域插槽有什么用
       vm.$scopedSlots = _parentVnode.data.scopedSlots || emptyObject
     }
 
     // set parent vnode. this allows render functions to have access
     // to the data on the placeholder node.
+    // 设置父Vnode。这允许渲染函数访问占位符节点上的数据。
     vm.$vnode = _parentVnode
-    // render self
+    // render self // 渲染和返回
     let vnode
     try {
+      //  调用render渲染，传入渲染代理和 init的时候组装的createElement
       vnode = render.call(vm._renderProxy, vm.$createElement)
     } catch (e) {
-      handleError(e, vm, `render`)
+      handleError(e, vm, `render`) // 错误处理工具
       // return error render result,
       // or previous vnode to prevent render error causing blank component
       /* istanbul ignore else */
+      // 区别环境进行错误渲染，或者储存vnode=父级vnode
       if (process.env.NODE_ENV !== 'production') {
         if (vm.$options.renderError) {
           try {
@@ -117,7 +124,7 @@ export function renderMixin (Vue: Class<Component>) {
       }
       vnode = createEmptyVNode()
     }
-    // set parent
+    // set parent // 储存vnode和设置parent
     vnode.parent = _parentVnode
     return vnode
   }
--- frame_learn/vue2x/src/core/vdom/create-element.js
+++ frame_learn/vue2x/src/core/vdom/create-element.js
@@ -23,22 +23,25 @@ import {
 const SIMPLE_NORMALIZE = 1
 const ALWAYS_NORMALIZE = 2
 
+// 包装函数，用于提供更灵活的接口
 // wrapper function for providing a more flexible interface
-// without getting yelled at by flow
+// without getting yelled at by flow // 翻译不出来
 export function createElement (
-  context: Component,
-  tag: any,
-  data: any,
-  children: any,
-  normalizationType: any,
-  alwaysNormalize: boolean
+  context: Component, // 上下文组件
+  tag: any, // 标签
+  data: any, // 数据
+  children: any, // 子
+  normalizationType: any, // 标准化啥？
+  alwaysNormalize: boolean // 总是标准的（不用再编译的）
 ): VNode | Array<VNode> {
+  // 如果是一个数组或者是基本类型，无data
   if (Array.isArray(data) || isPrimitive(data)) {
     normalizationType = children
     children = data
     data = undefined
   }
   if (isTrue(alwaysNormalize)) {
+    // 总是标准的normalizationType = 2 在 $createElement 下为true
     normalizationType = ALWAYS_NORMALIZE
   }
   return _createElement(context, tag, data, children, normalizationType)
@@ -51,7 +54,9 @@ export function _createElement (
   children?: any,
   normalizationType?: number
 ): VNode | Array<VNode> {
-  if (isDef(data) && isDef((data: any).__ob__)) {
+  // __ob__ 是啥 ，为什么如果存在就报错 然后返回空vnode
+  // data type == any
+  if (isDef(data) && isDef((data).__ob__)) {
     process.env.NODE_ENV !== 'production' && warn(
       `Avoid using observed data object as vnode data: ${JSON.stringify(data)}\n` +
       'Always create fresh vnode data objects in each render!',
@@ -60,14 +65,18 @@ export function _createElement (
     return createEmptyVNode()
   }
   // object syntax in v-bind
+  /** 异步v-bind处理，is在哪里赋值？ **/
   if (isDef(data) && isDef(data.is)) {
     tag = data.is
   }
+  // 无tag设置空节点
   if (!tag) {
+    // 在组件的情况下:设置为falsy值
     // in case of component :is set to falsy value
     return createEmptyVNode()
   }
   // warn against non-primitive key
+  // 警告不要使用非基本键 （非法键如obj对象）
   if (process.env.NODE_ENV !== 'production' &&
     isDef(data) && isDef(data.key) && !isPrimitive(data.key)
   ) {
@@ -80,28 +89,36 @@ export function _createElement (
     }
   }
   // support single function children as default scoped slot
+  // 支持单函数子函数作为默认作用域插槽
   if (Array.isArray(children) &&
     typeof children[0] === 'function'
   ) {
+    /** 此处修改作用域的默认有何用 ，其他children又如何处理？**/
     data = data || {}
     data.scopedSlots = { default: children[0] }
     children.length = 0
   }
+  // 总是规范化，递归铺平children树
   if (normalizationType === ALWAYS_NORMALIZE) {
     children = normalizeChildren(children)
+  // 简单规范化就铺平一级
   } else if (normalizationType === SIMPLE_NORMALIZE) {
     children = simpleNormalizeChildren(children)
   }
   let vnode, ns
   if (typeof tag === 'string') {
     let Ctor
+    // opts._parentvnode && ns存在 否则获取tagNamespace
+    /** 命名空间有什么用？？ **/
     ns = (context.$vnode && context.$vnode.ns) || config.getTagNamespace(tag)
+    // 是否html保留标签
     if (config.isReservedTag(tag)) {
-      // platform built-in elements
+      // platform built-in elements // 平台内置元素
       vnode = new VNode(
         config.parsePlatformTagName(tag), data, children,
         undefined, undefined, context
       )
+      // 如果是为已注册的组件名创建一个组件类型的vnode
     } else if (isDef(Ctor = resolveAsset(context.$options, 'components', tag))) {
       // component
       vnode = createComponent(Ctor, data, context, children, tag)
@@ -109,20 +126,23 @@ export function _createElement (
       // unknown or unlisted namespaced elements
       // check at runtime because it may get assigned a namespace when its
       // parent normalizes children
+      // 运行时检查未知或未列出的命名空间元素，因为当其父级规范化子级时，可能会为其分配命名空间。
       vnode = new VNode(
         tag, data, children,
         undefined, undefined, context
       )
     }
   } else {
+    // 直接组件选项/构造函数 // component类型直接调用createComponent创建
     // direct component options / constructor
     vnode = createComponent(tag, data, context, children)
   }
+  // 根据不同类型返回vnode （有何用？array和普通）
   if (Array.isArray(vnode)) {
     return vnode
   } else if (isDef(vnode)) {
     if (isDef(ns)) applyNS(vnode, ns)
-    if (isDef(data)) registerDeepBindings(data)
+    if (isDef(data)) registerDeepBindings(data) // resolve issues
     return vnode
   } else {
     return createEmptyVNode()
@@ -131,11 +151,14 @@ export function _createElement (
 
 function applyNS (vnode, ns, force) {
   vnode.ns = ns
+  // 不相干的标签对象
   if (vnode.tag === 'foreignObject') {
+    // 默认使用内部不相干对象的命名标签
     // use default namespace inside foreignObject
     ns = undefined
     force = true
   }
+  // 递归调用添加NameSpace
   if (isDef(vnode.children)) {
     for (let i = 0, l = vnode.children.length; i < l; i++) {
       const child = vnode.children[i]
@@ -150,6 +173,7 @@ function applyNS (vnode, ns, force) {
 // ref #5318
 // necessary to ensure parent re-render when deep bindings like :style and
 // :class are used on slot nodes
+// 当在槽节点上使用诸如：style和：class之类的深度绑定时，必须确保父级重新呈现
 function registerDeepBindings (data) {
   if (isObject(data.style)) {
     traverse(data.style)
